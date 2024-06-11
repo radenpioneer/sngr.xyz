41fcfa0eee5bc605f2a4df30662020f12dc85337
R. Ilham Sastronegoro <7355835+radenpioneer@users.noreply.github.com>
2024-06-11 10:04:40 +0000

    setup hybrid rendering

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,9 @@
 import { defineConfig } from 'astro/config';
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://www.sngr.xyz',
+  output: "hybrid",
+  adapter: cloudflare()
+});
