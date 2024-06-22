150ad71af315a7cc204d9c4f43fecad55e755582
R. Ilham Sastronegoro <7355835+radenpioneer@users.noreply.github.com>
2024-06-22 19:50:05 +0700

    DEV: Principal Site Development (#2)
    * WIP
    
    * WIP
    
    * WIP
    
    * WIP
    
    * reset
    
    * WIP
    
    * WIP
    
    * WIP

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,21 @@
-import { defineConfig } from 'astro/config';
-import cloudflare from "@astrojs/cloudflare";
+import { defineConfig } from 'astro/config'
+import { loadEnv } from 'vite'
+import react from '@astrojs/react'
+import markdoc from '@astrojs/markdoc'
+import keystatic from '@keystatic/astro'
+import cloudflare from '@astrojs/cloudflare'
+import tailwind from '@astrojs/tailwind'
+const { KEYSTATIC } = loadEnv(process.env.NODE_ENV, process.cwd(), '')
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://www.sngr.xyz',
-  output: "hybrid",
+  site: 'https://sngr.xyz',
+  integrations: [
+    react(),
+    markdoc(),
+    ...(KEYSTATIC ? [keystatic()] : []),
+    tailwind()
+  ],
+  output: 'hybrid',
   adapter: cloudflare()
-});
+})
