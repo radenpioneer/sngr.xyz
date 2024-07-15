1a16fc94ef3763dc38e90fc3427d206d4019021c
R. Ilham Sastronegoro <7355835+radenpioneer@users.noreply.github.com>
2024-07-15 15:13:31 +0000

    WIP

--- src/components/home/hero.tsx
+++ src/components/home/hero.tsx
@@ -14,9 +14,11 @@ export const HeroSection: FC<PropsWithChildren<HeroSectionProps>> = ({
 }) => (
   <section className='prose mt-8 flex min-h-screen max-w-none flex-col items-center gap-4 md:prose-lg md:justify-center md:gap-8'>
     <hgroup className='not-prose flex w-full flex-row-reverse items-center justify-end gap-4'>
-      <h1 className='text-2xl font-bold md:text-4xl'>{site.data.title}</h1>
+      <h1 className='bg-gradient-to-br from-green-500 to-purple-500 bg-clip-text text-2xl font-bold text-transparent md:text-4xl'>
+        {site.data.title}
+      </h1>
       <img
-        className='w-[35px] rounded-full md:w-[80px]'
+        className='w-[35px] rounded-full md:w-[50px]'
         src={logo.src}
         srcSet={logo.srcSet.attribute}
         {...logo.attributes}
