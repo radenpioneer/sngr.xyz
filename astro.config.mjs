a45489706f8300fe3da4835d5726b13c97e8695d
R. Ilham Sastronegoro <7355835+radenpioneer@users.noreply.github.com>
2024-08-08 09:14:41 +0000

    [skip ci] RESET

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,4 @@
-import { defineConfig } from 'astro/config'
-import { loadEnv } from 'vite'
-import react from '@astrojs/react'
-import markdoc from '@astrojs/markdoc'
-import keystatic from '@keystatic/astro'
-import cloudflare from '@astrojs/cloudflare'
-import tailwind from '@astrojs/tailwind'
-const { KEYSTATIC } = loadEnv(process.env.NODE_ENV, process.cwd(), '')
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({
-  site: 'https://sngr.xyz',
-  integrations: [
-    react(),
-    markdoc(),
-    tailwind(),
-    ...(KEYSTATIC ? [keystatic()] : [])
-  ],
-  output: 'hybrid',
-  adapter: cloudflare()
-})
+export default defineConfig({});
