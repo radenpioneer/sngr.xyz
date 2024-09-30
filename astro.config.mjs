dbe54750306e0cff1ca4c38ffacb05cc3bffddee
R. Ilham Sastronegoro <7355835+radenpioneer@users.noreply.github.com>
2024-09-30 09:19:59 +0000

    RESET

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,24 +1,12 @@
 import { defineConfig } from 'astro/config'
-import react from '@astrojs/react'
-import keystatic from '@keystatic/astro'
 import cloudflare from '@astrojs/cloudflare'
-import icons from 'unplugin-icons/vite'
-import tailwindcss from '@tailwindcss/vite'
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://www.sngr.xyz',
-  integrations: [react(), keystatic()],
   output: 'hybrid',
-  adapter: cloudflare(),
-  vite: {
-    plugins: [icons({ compiler: 'jsx', jsx: 'react' }), tailwindcss()],
-    define: {
-      'process.env': process.env
+  adapter: cloudflare({
+    platformProxy: {
+      enabled: true
     }
-  },
-  experimental: {
-    contentCollectionCache: true,
-    directRenderScript: true
-  }
+  })
 })
