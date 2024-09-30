8ed92fb884e41486f7a667a0716ba5c5aeb31580
radenpioneer <7355835+radenpioneer@users.noreply.github.com>
2024-09-30 10:16:12 +0000

    WIP

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,18 @@
 import { defineConfig } from 'astro/config'
 import cloudflare from '@astrojs/cloudflare'
-
 import react from '@astrojs/react'
+import keystatic from '@keystatic/astro'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
+  integrations: [react(), ...(process.env.KEYSTATIC ? [keystatic()] : [])],
   output: 'hybrid',
   adapter: cloudflare({
     platformProxy: {
       enabled: true
     }
-  })
+  }),
+  experimental: {
+    contentLayer: true
+  }
 })
