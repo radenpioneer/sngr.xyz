5d3490b9a999f366ada49d7f3cff398a4d34a0e9
R. Ilham Sastronegoro <7355835+radenpioneer@users.noreply.github.com>
2024-10-03 09:19:02 +0000

    add projects data schema

--- keystatic.config.mjs
+++ keystatic.config.mjs
@@ -1,10 +1,15 @@
 // @ts-check
 import { config } from '@keystatic/core'
 import { site } from '~/data/cms/site.mjs'
+import { projects } from '~/data/cms/projects.mjs'
 
 export default config({
   storage: {
     kind: 'local'
+  },
+
+  collections: {
+    projects
   },
 
   singletons: {
