073dae3e5e7386cf0ec84d04ead6ca8b823a1908
R. Ilham Sastronegoro <7355835+radenpioneer@users.noreply.github.com>
2024-10-19 09:43:07 +0000

    Setup deployment action & code formatter

---
+++ prettier.config.cjs
@@ -0,0 +1,19 @@
+// @ts-check
+/** @type {import("prettier").Config} */
+module.exports = {
+  // i am just using the standard config, change if you need something else
+  ...require('prettier-config-standard'),
+  plugins: [
+    require.resolve('prettier-plugin-astro'),
+    require.resolve('prettier-plugin-tailwindcss')
+  ],
+  overrides: [
+    {
+      files: '*.astro',
+      options: {
+        parser: 'astro',
+        htmlWhitespaceSensitivity: 'ignore'
+      }
+    }
+  ]
+}
