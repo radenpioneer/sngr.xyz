a60eb463753aed4bd0d664abdcbb08272afdf29f
R. Ilham Sastronegoro <7355835+radenpioneer@users.noreply.github.com>
2024-11-06 14:40:44 +0000

    setup vite-pwa

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'astro/config'
 import cloudflare from '@astrojs/cloudflare'
 import react from '@astrojs/react'
 import tailwind from '@astrojs/tailwind'
+import pwa from '@vite-pwa/astro'
 import keystatic from '@keystatic/astro'
 import icons from 'unplugin-icons/vite'
 
@@ -11,6 +12,9 @@ export default defineConfig({
   integrations: [
     react(),
     tailwind(),
+    pwa({
+      registerType: 'autoUpdate'
+    }),
     ...(process.env.KEYSTATIC ? [keystatic()] : [])
   ],
   output: 'hybrid',
