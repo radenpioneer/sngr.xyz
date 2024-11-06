8185c7a4bdb37f3083ea55d7db7eac2486adfa13
R. Ilham Sastronegoro <7355835+radenpioneer@users.noreply.github.com>
2024-11-06 22:56:25 +0700

    View Transition & App-Like Navigation (#22)
    * implement view transition
    
    * setup pwa-assets-generator, wireit
    
    * setup vite-pwa
    
    * remove bun-version
    
    * configure manifest
    
    * set `start_url` and `display`
    
    * set `display` to `minimal-ui`

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'astro/config'
 import cloudflare from '@astrojs/cloudflare'
 import react from '@astrojs/react'
 import tailwind from '@astrojs/tailwind'
+import pwa from '@vite-pwa/astro'
 import keystatic from '@keystatic/astro'
 import icons from 'unplugin-icons/vite'
 
@@ -11,6 +12,13 @@ export default defineConfig({
   integrations: [
     react(),
     tailwind(),
+    pwa({
+      registerType: 'autoUpdate',
+      manifest: false,
+      workbox: {
+        maximumFileSizeToCacheInBytes: 3145728
+      }
+    }),
     ...(process.env.KEYSTATIC ? [keystatic()] : [])
   ],
   output: 'hybrid',
